3f4aa4b238fc7410704adf12b4b82b822534939f
Yinn <1499978024@qq.com>
2022-03-31 17:44:43 +0200

    087

---
+++ vue脚手架部分/src/components/Student.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="student">
+        <h2>Student Name: {{name}}</h2>
+        <h2>Student Sex: {{sex}}</h2>
+        <button @click="sendStudentName">Pass the name to the school</button>
+    </div>
+</template>
+
+<script>
+    import pubsub from 'pubsub-js'
+    
+    export default {
+        name: 'Student',
+        data() {
+            return {
+                name: 'San',
+                sex: 'M',
+            }
+        },
+        mounted() {
+
+        },
+        methods: {
+            sendStudentName(){
+                // this.$bus.$emit('hello', this.name)
+                pubsub.publish('hello', 666)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .student{
+        background-color: pink;
+        margin-top: 30px;
+        padding: 5px;
+    }
+</style>
